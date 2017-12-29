3ef0c50def427b90cf7c65c5f57c7fbdd08c73fa
Kyle Ruschel <dkr1985@icloud.com>
2017-12-28 20:35:55 -0500

    updates

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 let PORT = process.env.PORT || 3000;
 
-require("routes/apiroutes.js")(app);
+require("/routes/apiroutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App is listening on Port " + PORT);
