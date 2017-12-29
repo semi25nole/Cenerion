6c92cbec1bae35b8fe6241ce614c216b9610115b
Kyle Ruschel <dkr1985@icloud.com>
2017-12-28 20:19:42 -0500

    updates

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 let express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 let port = process.env.port || 3000;
 
 app.get("/", function(req, res) {
-    JSON.stringify(res);
-})
+    res.sendFile(path.join(__dirname, "index.html")); //base page
+});
+
+app.get("/movie", function(req, res) {
+    res.sendFile(path.join(__dirname, "main.html"));
+});
 
 app.listen(port, function() {
     console.log("App is listening on Port " + port);
