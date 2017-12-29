abf2934114bb27e1e391a767e2578aa46a938a1f
Kyle Ruschel <dkr1985@icloud.com>
2017-12-28 19:42:09 -0500

    updating

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ let app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", function(req, res) {
-    res.send(res);
-});
-
 app.listen(port, function() {
     console.log("App is running on port " + port);
 });
