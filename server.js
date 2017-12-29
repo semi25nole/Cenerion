e050fbe40a6e597dabb9645037ceadcb027e06fc
Kyle Ruschel <dkr1985@icloud.com>
2017-12-28 20:08:52 -0500

    updates

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 let express = require("express");
+
 let app = express();
 
 let port = process.env.port || 3000;
 
 app.get("/", function(req, res) {
-    res.stringify(res);
+    JSON.stringify(res);
 })
+
+app.listen(port, function() {
+    console.log("App is listening on Port " + port);
+});
+
